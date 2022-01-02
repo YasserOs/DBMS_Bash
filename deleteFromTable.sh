21730ad989dc3407f413bf2678ce2be5f78fd8f0
Muhammed Gamal <muhammedgamal760@gmail.com>
2022-01-02 21:50:20 +0200

    delete

--- deleteFromTable.sh
+++ deleteFromTable.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
+typeset -i fn=1
+typeset -i column_found=0
+typeset -i current_record=4
 read -p "Enter name of table : " tablename
-#read -p "Enter name of primary key : " pkname
-read -p "Enter the record number you want to delete : " recnum
-if [ -f $tablename ]
+typeset -i fieldnum=$(head -n 1 $dbPath/$DBdir/$tablename)
+if [ -f $dbPath/$DBdir/$tablename ]
     then
-        awk -F: '{
-                    if(FNR==recnum)
-                        {
-                            rm $0
-                        } 
-                }'
-else
-    read -p "Table doesn't exist >>
-press 0 to go to previous menu
-or 1 to go back to main menu 
-or 2 to retry :" x
-            case $x in
-            0) calledFromMenu=0; 
-               source ./connectDatabase.sh
-                ;;
-            1) source ./Menu.sh
-                ;;
-            2) source ./deleteFromTable.sh
-                ;;
-            esac
+    while true
+    do
+        read -p "Enter name of column : " columnname
+        while test $fn -le $fieldnum
+        do
+            if [ "$columnname" == "$(head -n $fn $dbPath/$DBdir/$tablename | tail -n 1 | cut -d: -f$fn)" ]
+            then
+            column_found=1
+            break
+            else
+            column_found=0
+            fi
+            fn=$fn+1
+        done
+        if [ $column_found -eq 1 ]
+        then
+            break
+        else
+            echo "Column name does not exist!"
+        fi
+    done
+    read -p "Enter value : " x
+    lines=$(cat $dbPath/$DBdir/$tablename | wc -l)
+    while test $current_record -le $lines
+    do
+        match_column=`cat $dbPath/$DBdir/$tablename | head -n $current_record | tail -n 1 | cut -d: -f$fn`
+        if [ "$match_column" == "$x" ]
+        then
+            match_record=`cat $dbPath/$DBdir/$tablename | head -n $current_record | tail -n 1`
+            sed -i '/'"$match_record"'/d' $dbPath/$DBdir/$tablename
+            current_record=$current_record-1
+        fi
+        current_record=$current_record+1
+    done
 fi
+    
+        
+        
+        
+        
+ 
