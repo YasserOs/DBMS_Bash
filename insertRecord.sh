bc58e6c6b1b2f8820cd54994b513de4f6a906e3f
Muhammed Gamal <muhammedgamal760@gmail.com>
2022-01-02 19:18:17 +0200

    finally inserted

--- insertRecord.sh
+++ insertRecord.sh
@@ -3,10 +3,11 @@ shopt -s extglob
 export LC_COLLATE=C 
 typeset -i i=2 ## i starts from 2nd line as th 1st contains number of columns
 typeset -i fn=1 ## field number
+typeset -i pk_found=0
 let columns_array[0]
 let datatypes_array[0]
 let pk_array[0]
-input_record=" "
+input_record=""
 read -p "Enter table name : " tbname
 if [ -f $dbPath/$DBdir/$tbname ]
         then
@@ -33,11 +34,12 @@ if [ -f $dbPath/$DBdir/$tbname ]
         
         file=$(cat $dbPath/$DBdir/$tbname | wc -l)
         rec_num=$(( file - 3 ))
-        records=$(tail -n $rec_num $dbPath/$DBdir/$tbname)
+        records=$(tail -n $rec_num $dbPath/$DBdir/$tbname > temp_records)
         typeset -i r=1
         while test $r -le $rec_num
             do
-                pk_array[$r]= head -n $r $records | tail -n 1 
+            #cat temp_records
+                pk_array[$r]=$(head -$r temp_records | tail -1 | cut -d: -f1) 
                 r=$r+1
             done
 
@@ -48,12 +50,40 @@ if [ -f $dbPath/$DBdir/$tbname ]
     do
         while true 
         do  
-            if [ $n -eq 0 ]
             read -p "enter data for ${columns_array[$n]} with type ${datatypes_array[$n]} : "  x
+
+            ### primary key validation
+
+                    if [ $n -eq 0 ]
+                    then
+                        for i in "${pk_array[@]}"
+                        do
+                            if [ "$x" == "$i" ]
+                            then
+                                echo "primary key $i already exists!"
+                                pk_found=1
+                                break
+                            else
+                                pk_found=0
+                            fi
+                        done
+                        if [ $pk_found -eq 1 ]
+                        then
+                            continue
+                        fi
+                    fi
+                    
+            ### end of validation
+
                     if [ "${datatypes_array[$n]}" == "String" ]
                         then
                             case $x in 
-                                +([a-zA-Z])) input_record="$input_record:$x";
+                                +([a-zA-Z])) if [ $n -eq 0 ] 
+                                             then 
+                                                input_record="$x"
+                                             else 
+                                             input_record="$input_record:$x"
+                                             fi;
                                              break
                                              ;;
                                         *)  echo "please enter a string without any special characters" 
@@ -64,9 +94,14 @@ if [ -f $dbPath/$DBdir/$tbname ]
                     elif [ "${datatypes_array[$n]}" == "Int" ]
                         then
                             case $x in 
-                                +([0-9])) input_record="$input_record:$x";
-                                           break
-                                           ;;
+                                +([0-9])) if [ $n -eq 0 ] 
+                                          then 
+                                          input_record="$x"
+                                          else 
+                                          input_record="$input_record:$x"
+                                          fi;
+                                          break
+                                          ;;
                                      *) echo "please enter an integer!"
                                         continue
                                            ;; 
@@ -76,11 +111,15 @@ if [ -f $dbPath/$DBdir/$tbname ]
             n=$n+1
     done
     
-echo $input_record
+echo $input_record >> $dbPath/$DBdir/$tbname
+rm -r temp_records
+echo "=============================="
+echo "Record inserted successfully!"
+echo "=============================="
 sleep 3
 calledFromMenu=0
 source ./connectDatabase.sh
-                        
+       
 else
     read -p "Table doesn't exist , 
     press 0 to go back to previous menu 
